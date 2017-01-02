bf7bce8731a51e79d710d4a95f4b0ca660dec7d8
barrenWuffet <sargow@yahoo.com>
2017-01-02 12:09:44 -0500

    add location csv
    Signed-off-by: barrenWuffet <sargow@yahoo.com>

---
+++ src/R/20170102_get_wunderground_json.R
@@ -0,0 +1,31 @@
+
+rm(list = ls(all = T))
+gc()
+
+library(RJSONIO)
+
+
+
+file.choose()
+loc1 <- read.csv()
+
+
+
+raw1 <- fromJSON('http://api.wunderground.com/api/1ead1116c63dc43a/geolookup/conditions/q/NJ/Bloomfield.json')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
